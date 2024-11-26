10c481e19e622acf485091f8f3e453037635b677
ojefersoncode <jefersonvidal999@gmail.com>
2024-11-26 05:02:02 -0300

    test

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"; // Import do Firestore
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBEYVGzmzmxE9R2JJKhvs32khSVoS3cszY",
+  authDomain: "robok-website.firebaseapp.com",
+  projectId: "robok-website",
+  storageBucket: "robok-website.firebasestorage.app",
+  messagingSenderId: "608417068126",
+  appId: "1:608417068126:web:4a27aa3cd9f35bef20b0cf",
 };
 
 // Inicializa o Firebase
@@ -17,6 +17,3 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 export { db };
-
-
-
